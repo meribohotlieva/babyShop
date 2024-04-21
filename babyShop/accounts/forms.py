756a8284b1d21a7dba44a72457471f5807d4d7f4
meribohotlieva <140341925+meribohotlieva@users.noreply.github.com>
2024-04-21 08:03:25 +0300

    Update forms.py

--- babyShop/accounts/forms.py
+++ babyShop/accounts/forms.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
+from django import forms
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = ('username', 'email')
+
+
+class CustomPasswordResetForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
